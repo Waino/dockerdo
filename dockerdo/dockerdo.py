f44379ea0afd87a2086b263887182938d2ceb34a
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2025-03-14 22:13:49 +0200

    Subcommands

--- dockerdo/dockerdo.py
+++ dockerdo/dockerdo.py
@@ -15,6 +15,67 @@ def cli():
 @click.option('--no_bashrc', is_flag=True, help="Do not modify ~/.bashrc")
 @cli.command()
 def install(no_bashrc: bool):
+    """Install dockerdo"""
+    return 0
+
+
+@cli.command()
+def init():
+    """Initialize dockerdo"""
+    return 0
+
+
+@cli.command()
+def overlay():
+    """Overlay a Dockerfile with the changes needed by dockerdo"""
+    return 0
+
+
+@cli.command()
+def build():
+    """Build a Docker image"""
+    return 0
+
+
+@cli.command()
+def push():
+    """Push a Docker image"""
+    return 0
+
+
+@cli.command()
+def start():
+    """Start the container"""
+    return 0
+
+
+@cli.command()
+def export():
+    """Add an environment variable to the env list"""
+    return 0
+
+
+@cli.command()
+def run():
+    """Run a command in the container"""
+    return 0
+
+
+@cli.command()
+def stop():
+    """Stop the container"""
+    return 0
+
+
+@cli.command()
+def history():
+    """Show the history of a container"""
+    return 0
+
+
+@cli.command()
+def rm():
+    """Remove a container"""
     return 0
 
 
