2bd2a0bbf352dfd2600dccd97c0c7def56568188
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2025-04-02 22:55:24 +0300

    Test image name with org

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -20,6 +20,7 @@ def test_ephemeral_container_name():
         ("harbor.local", "alpine", "foobar", "harbor.local/dockerdo-alpine:latest-foobar"),
         ("", "alpine:3.14", "test", "dockerdo-alpine:3.14-test"),
         (None, "custom/image:tag", "test-123", "dockerdo-image:tag-test-123"),
+        (None, "custom/org/image:tag", "test-456", "dockerdo-image:tag-test-456"),
     ],
 )
 def test_make_image_tag(registry, base_image, session_name, expected):
