15144318e44394a6d93a74f860406a7f6f2067c9
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2025-04-29 21:06:33 +0300

    Release v0.2.1

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -eux
 # Update the bash completion
 _DOCKERDO_COMPLETE=bash_source dockerdo > dockerdo/dockerdo.bash-completion
 
+# Remove old build artefacts, to force rebuild
+rm dist/*.whl dist/*.tar.gz
+
 # Build the package
 flit build
